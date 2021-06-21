954f05ef1ec547a46f013ed5f4e4d1bc02128fd8
Boris Kontorovich <boris@kontorovich.com>
2021-06-20 21:50:37 -0400

    GPIO working.

--- gpio.py
+++ gpio.py
@@ -1,5 +1,27 @@
 import board
-from digitalio import DigitalInOut
-from adafruit_vl53l0x import VL53L0X
+from digitalio import DigitalInOut, Direction, Pull
+import busio
+import adafruit_vl53l0x
 
-i2c = board.I2C()
+import time
+
+
+led = DigitalInOut(board.D4)
+led.direction = Direction.OUTPUT
+
+led_state = False
+
+i2c = busio.I2C(board.SCL, board.SDA)
+vl53 = adafruit_vl53l0x.VL53L0X(i2c)
+
+while True:
+
+    if led_state == False:
+        led_state = True
+    elif led_state == True:
+        print(f"Range {vl53.range}")
+        led_state = False
+
+    led.value = led_state
+        
+    time.sleep(1)
